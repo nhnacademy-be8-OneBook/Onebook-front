0cfdcf9c0a6bd5292567b34d0e161d51c0ea17a3
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2024-12-11 11:01:14 +0900

    create front-api

---
+++ frontapi/src/main/java/com/onebook/frontapi/FrontapiApplication.java
@@ -0,0 +1,13 @@
+package com.onebook.frontapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontapiApplication.class, args);
+	}
+
+}
