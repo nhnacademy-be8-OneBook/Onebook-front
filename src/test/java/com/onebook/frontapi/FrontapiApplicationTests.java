512a0283c3b0d926aca5fc1d67fb57453656eb5e
Joo-v7 <helloworld.v7@gmail.com>
2024-12-31 16:12:13 +0900

    test 코드 주석처리.

--- src/test/java/com/onebook/frontapi/FrontapiApplicationTests.java
+++ src/test/java/com/onebook/frontapi/FrontapiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.onebook.frontapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FrontapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.onebook.frontapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FrontapiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
