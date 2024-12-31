385a9ce1daa57cda3cdd625955b642b48dfd4a8f
Joo-v7 <helloworld.v7@gmail.com>
2024-12-31 15:54:12 +0900

    회원가입 폼 완료.

--- src/main/java/com/onebook/frontapi/feign/member/MemberClient.java
+++ src/main/java/com/onebook/frontapi/feign/member/MemberClient.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "${onebook.taskName}", url="${onebook.gatewayUrl}")
+@FeignClient(name = "memberClient", url="localhost:8210/task/members")
+//@FeignClient(name = "${onebook.taskName}", url="${onebook.gatewayUrl}")
 public interface MemberClient {
 
     @PostMapping
