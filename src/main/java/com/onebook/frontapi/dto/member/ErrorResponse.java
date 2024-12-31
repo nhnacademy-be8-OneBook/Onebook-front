88119f73586ce7f7966325f43b9ad6cb8158053b
Joo-v7 <helloworld.v7@gmail.com>
2024-12-31 16:50:56 +0900

    jsonignoreproperties 추가.

--- src/main/java/com/onebook/frontapi/dto/member/ErrorResponse.java
+++ src/main/java/com/onebook/frontapi/dto/member/ErrorResponse.java
@@ -1,13 +1,14 @@
 package com.onebook.frontapi.dto.member;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor
 @Setter
 @Getter
