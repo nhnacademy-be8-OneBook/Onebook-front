7e0527efd98739e40445e6d30356a51dc8833bf7
pangpange <eric0420@naver.com>
2025-01-02 18:08:26 +0900

    feat/배송지 등록기능

--- src/main/java/com/onebook/frontapi/controller/address/AddressController.java
+++ src/main/java/com/onebook/frontapi/controller/address/AddressController.java
@@ -1,14 +1,35 @@
 package com.onebook.frontapi.controller.address;
 
+import com.onebook.frontapi.dto.address.request.AddMemberAddressRequest;
+import com.onebook.frontapi.dto.address.response.AddMemberAddressResponse;
+import com.onebook.frontapi.service.address.AddressService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AddressController {
 
+    private final AddressService addressService;
+
+    // 배송지 등록 페이지 불러오기
     @GetMapping("/address/register-form")
     public String getAddressRegisterForm(){
         return "/address/address-register-form";
+    }
+
+    // 배송지 등록 기능
+    @PostMapping("/address/register")
+    public String registerAddress(@ModelAttribute AddMemberAddressRequest addMemberAddressRequest){
+
+        addressService.addMemberAddress(addMemberAddressRequest);
+
+        System.out.println("잘됏음!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        return null;
     }
 
 }
