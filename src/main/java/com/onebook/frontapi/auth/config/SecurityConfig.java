38299cd875215add79b1d1a26fb2133fbacbd2c7
bbangho <dudgh614@gmail.com>
2025-01-03 10:54:36 +0900

    dev관련 파일들 삭제

--- src/main/java/com/onebook/frontapi/auth/config/SecurityConfig.java
+++ src/main/java/com/onebook/frontapi/auth/config/SecurityConfig.java
@@ -26,84 +26,46 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
-    private final MemberFeignClient memberFeignClient;
     private final AuthFeignClient authFeignClient;
-    private final Environment environment;
-
 
     @Bean
     public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
 
 
-        if (isDevProfile()) {
-            // 개발 중일 때는 모든 요청을 허용
-            log.info("dev profile ");
-            http.authorizeHttpRequests(authRequest ->
-                    authRequest.anyRequest().permitAll());
+        http.formLogin(formLogin ->
+                formLogin
+                        .loginPage("/auth/login") // 사용자 정의 로그인 페이지
+                        .usernameParameter("id") // 사용자명 파라미터 이름
+                        .passwordParameter("password") // 비밀번호 파라미터 이름
+                        .loginProcessingUrl("/login/process") // 로그인 처리 URL
+                        .successHandler(new AuthSuccessHandler(authFeignClient))// jwt token 추가하기
+                        .permitAll() // 로그인 페이지 접근 허용
+        );
+        // dev 외의 환경에서는 제한된 접근만 허용
+        http.authorizeHttpRequests(authRequest -> {
+            authRequest.requestMatchers("/", "/auth/login", "/public/**",
+                            "/css/**", "/js/**", "/images/**", "/join", "/test/**",
+                            "/style.css").permitAll()
+                    .anyRequest().authenticated();
+        });
 
-        } else {
-            log.info("not dev ");
-            http.formLogin(formLogin ->
-                            formLogin
-                                    .loginPage("/auth/login") // 사용자 정의 로그인 페이지
-                                    .usernameParameter("id") // 사용자명 파라미터 이름
-                                    .passwordParameter("password") // 비밀번호 파라미터 이름
-                                    .loginProcessingUrl("/login/process") // 로그인 처리 URL
-                                    .successHandler(new AuthSuccessHandler(authFeignClient))// jwt token 추가하기
-                                    .permitAll() // 로그인 페이지 접근 허용
-            );
-            // dev 외의 환경에서는 제한된 접근만 허용
-            http.authorizeHttpRequests(authRequest -> {
-                authRequest.requestMatchers("/", "/auth/login", "/public/**",
-                                "/css/**", "/js/**", "/images/**", "/join", "/test/**",
-                                "/style.css").permitAll()
-                        .anyRequest().authenticated();
-            });
-
-            // logout 시 쿠키 삭제하기
-            http.logout( logout -> {
-                logout.logoutUrl("/auth/logout").
-                        deleteCookies("Authorization");
-            });
-        }
+        // logout 시 쿠키 삭제하기
+        http.logout(logout -> {
+            logout.logoutUrl("/auth/logout").
+                    deleteCookies("Authorization");
+        });
 
         return http.build();
     }
 
-    private boolean isDevProfile() {
-        return Arrays.asList(environment.getActiveProfiles()).contains("dev");
-    }
 
     // Password Encoder, InMemory is Dev
     @Bean
-//    @Profile("dev")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // BCrypt 암호화 사용
+
     }
-
-
-
-    @Bean
-    @Profile("dev")
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
-        // 사용자 정보 생성
-        UserDetails user1 = User.builder()
-                .username("user1")
-                .password(passwordEncoder.encode("password1")) // 비밀번호 인코딩
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("password2"))
-                .roles("ADMIN")
-                .build();
-
-        // InMemoryUserDetailsManager에 사용자 추가
-        return new InMemoryUserDetailsManager(user1, admin);
-    }
-
 
 
 }
