5ec956cff4a4873a0b7b0bb8f9c3d7b17672a8ee
pangpange <eric0420@naver.com>
2025-01-04 18:57:19 +0900

    feat/배송지 수정기능

--- src/main/java/com/onebook/frontapi/service/address/AddressService.java
+++ src/main/java/com/onebook/frontapi/service/address/AddressService.java
@@ -2,6 +2,7 @@ package com.onebook.frontapi.service.address;
 
 import com.onebook.frontapi.dto.address.request.AddMemberAddressRequest;
 import com.onebook.frontapi.dto.address.request.DeleteMemberAddressRequest;
+import com.onebook.frontapi.dto.address.request.UpdateMemberAddressRequest;
 import com.onebook.frontapi.dto.address.response.MemberAddressResponse;
 import com.onebook.frontapi.feign.address.AddressClient;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,16 @@ public class AddressService {
         return addressClient.addMemberAddress(addMemberAddressRequest).getBody();
     }
 
+    public MemberAddressResponse getMemberAddressById(Long id){
+        return addressClient.getMemberAddressById(id).getBody();
+    }
+
     public List<MemberAddressResponse> getAllMemberAddressByMemberId() {
         return addressClient.getAllMemberAddressByMemberId().getBody();
+    }
+
+    public MemberAddressResponse updateMemberAddress(UpdateMemberAddressRequest updateMemberAddressRequest){
+        return addressClient.updateMemberAddressById(updateMemberAddressRequest).getBody();
     }
 
     public MemberAddressResponse deleteMemberAddress(DeleteMemberAddressRequest deleteMemberAddressRequest){
