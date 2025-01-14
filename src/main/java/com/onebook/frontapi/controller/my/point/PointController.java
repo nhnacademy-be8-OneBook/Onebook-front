b2655f35e5b22e399dbecb8c9670da989aa52e55
ParkSuYeon <tndus165@naver.com>
2025-01-14 14:27:21 +0900

    포인트 화면 출력하기

--- src/main/java/com/onebook/frontapi/controller/my/point/PointController.java
+++ src/main/java/com/onebook/frontapi/controller/my/point/PointController.java
@@ -1,15 +1,15 @@
 package com.onebook.frontapi.controller.my.point;
 
-import com.onebook.frontapi.dto.point.MemberPointResponse;
+import com.onebook.frontapi.dto.point.response.PointLogResponse;
 import com.onebook.frontapi.service.point.PointService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/my/point")
@@ -22,14 +22,13 @@ public class PointController {
         this.pointService = pointService;
     }
 
-    @GetMapping("/mypoints")  // "/mypoints" 경로
+    @GetMapping("/point")
     public ModelAndView getUserPointHistories(HttpSession session) {
-        // 로그인 체크를 하지 않고 포인트 내역을 조회할 수 있도록 변경
-        List<MemberPointResponse> memberPointHistories = pointService.getMemberPointHistories();
+        Page<PointLogResponse> memberPointHistories = pointService.getMemberPointHistories();
 
-        // 포인트 내역을 my/point/mypoints 뷰로 전달
-        ModelAndView modelAndView = new ModelAndView("my/point/mypoints");
+        ModelAndView modelAndView = new ModelAndView("my/point/point");
         modelAndView.addObject("memberPointHistories", memberPointHistories);
+
         return modelAndView;
     }
 }
--- src/main/java/com/onebook/frontapi/service/point/PointService.java
+++ src/main/java/com/onebook/frontapi/service/point/PointService.java
@@ -1,46 +1,10 @@
 package com.onebook.frontapi.service.point;
 
-import com.onebook.frontapi.common.CommonResponse;
-import com.onebook.frontapi.dto.point.MemberPointResponse;
-import com.onebook.frontapi.feign.point.PointFeignClient;
-import feign.FeignException;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
+import com.onebook.frontapi.dto.point.response.PointLogResponse;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
-
-@Service
-@Transactional(readOnly = true)
-public class PointService {
-
-    private final PointFeignClient pointFeign;
-
-    public PointService(PointFeignClient pointFeign) {
-        this.pointFeign = pointFeign;
-    }
-
-    // PointService에서 포인트 내역 가져오기
-    public List<MemberPointResponse> getMemberPointHistories() {
-        try {
-            CommonResponse<List<MemberPointResponse>> response = pointFeign.getUserPointHistories();
-
-            if (response == null || response.getResult() == null) {
-                throw new IllegalStateException("잘못된 상태: 포인트 내역을 찾을 수 없음");
-            }
-
-            return response.getResult();
-        } catch (FeignException.Unauthorized ex) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인되지 않은 상태에서 포인트 내역을 조회할 수 없습니다.", ex);
-        } catch (FeignException.BadRequest ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", ex);
-        } catch (FeignException.NotFound ex) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "포인트 내역을 찾을 수 없습니다.", ex);
-        } catch (FeignException.InternalServerError ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류", ex);
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다.", ex);
-        }
-    }
+public interface PointService {
+    // 포인트 내역 가져오기
+    Page<PointLogResponse> getMemberPointHistories();
 }
+
