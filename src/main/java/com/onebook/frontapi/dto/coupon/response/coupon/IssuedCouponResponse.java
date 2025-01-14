b7cd6ca972ad7fdf2588cd313b12d523e1ff4b64
pangpange <eric0420@naver.com>
2025-01-14 11:17:10 +0900

    사용자 쿠폰함 조회 기능

--- src/main/java/com/onebook/frontapi/dto/coupon/response/coupon/IssuedCouponResponse.java
+++ src/main/java/com/onebook/frontapi/dto/coupon/response/coupon/IssuedCouponResponse.java
@@ -1,11 +1,11 @@
 package com.onebook.frontapi.dto.coupon.response.coupon;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
+@Setter
 @Getter
 public class IssuedCouponResponse {
 
@@ -14,4 +14,29 @@ public class IssuedCouponResponse {
     private Long memberId;
     private LocalDateTime issueDateTime;
     private LocalDateTime useDateTime;
+
+    // 쿠폰의 타입
+    private String couponType;
+
+    // 할인관련 정보
+    private Integer minimumOrderAmount;
+    private Integer discountRate;
+    private Integer discountPrice;
+    private Integer maximumDiscountPrice;
+
+    // 유효기간 정보
+    private LocalDateTime expirationPeriodStart;
+    private LocalDateTime expirationPeriodEnd;
+
+    // 쿠폰정책의 기본정보
+    private String name;
+    private String description;
+
+    // 쿠폰의 적용대상에 대한 정보
+    private String bookName;
+    private String bookIsbn13;
+    private String categoryName;
+
+
+
 }
