1ebcaaac5f1a1f74e4775d0273aa9a4fb8990300
Joo-v7 <helloworld.v7@gmail.com>
2025-01-15 00:46:05 +0900

    마이 페이지 홈에서 주문 내역 한 눈에 볼 수 있게.

--- src/main/java/com/onebook/frontapi/controller/my/home/MyHomeController.java
+++ src/main/java/com/onebook/frontapi/controller/my/home/MyHomeController.java
@@ -1,12 +1,18 @@
 package com.onebook.frontapi.controller.my.home;
 
 import com.onebook.frontapi.dto.member.MemberResponse;
+import com.onebook.frontapi.dto.myhome.MyOrderStatusResponse;
+import com.onebook.frontapi.dto.order.OrderResponse;
 import com.onebook.frontapi.service.member.MemberService;
+import com.onebook.frontapi.service.myhome.MyHomeService;
+import com.onebook.frontapi.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,11 +20,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyHomeController {
 
     private final MemberService memberService;
+    private final OrderService orderService;
+    private final MyHomeService myHomeService;
 
     @GetMapping("/home")
     public String myHome(Model model) {
         MemberResponse memberResponse = memberService.getMember();
+        List<OrderResponse> orderResponses = orderService.getAllOrders();
+        MyOrderStatusResponse myOrderStatusResponse = myHomeService.getMyOrderStatus(orderResponses);
+
         model.addAttribute("member", memberResponse);
+        model.addAttribute("myOrderStatus", myOrderStatusResponse);
         return "my/home/home";
     }
 
