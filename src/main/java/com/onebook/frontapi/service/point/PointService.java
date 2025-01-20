24dc25cc010bc8fbaf8c270e87b8cd11b3489384
tnvs99 <tnvs99@gmail.com>
2025-01-20 20:28:02 +0900

    Update myPage 현재 포인트 확인가능

--- src/main/java/com/onebook/frontapi/service/point/PointService.java
+++ src/main/java/com/onebook/frontapi/service/point/PointService.java
@@ -1,8 +1,13 @@
 package com.onebook.frontapi.service.point;
 
 import com.onebook.frontapi.dto.point.response.MemberPointResponse;
+import com.onebook.frontapi.dto.point.response.PointLogResponse;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 
 public interface PointService {
     List<MemberPointResponse> getMemberPointHistories(Long memberId);
+
+    int getMemberPoint();
 }
