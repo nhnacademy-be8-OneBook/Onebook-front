15daffef517d2ce38fe4b4b4fbf36ef8d9893f31
Joo-v7 <helloworld.v7@gmail.com>
2025-01-22 02:42:01 +0900

    admin page - grade

---
+++ src/main/java/com/onebook/frontapi/controller/role/RoleAdminController.java
@@ -0,0 +1,12 @@
+package com.onebook.frontapi.controller.role;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/admin/roles")
+public class RoleAdminController {
+
+}
--- src/main/java/com/onebook/frontapi/feign/grade/GradeClient.java
+++ src/main/java/com/onebook/frontapi/feign/grade/GradeClient.java
@@ -1,9 +1,13 @@
 package com.onebook.frontapi.feign.grade;
 
 import com.onebook.frontapi.dto.grade.GradeFeignResponse;
+import com.onebook.frontapi.dto.grade.GradeModifyRequest;
+import com.onebook.frontapi.dto.grade.GradeRegisterRequest;
+import com.onebook.frontapi.dto.grade.GradeResponseForAdmin;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,4 +18,28 @@ public interface GradeClient {
 
     @GetMapping("/task/grades/list")
     List<GradeFeignResponse> getAllRequest();
+
+    /**
+     * 관리자 - 등급 단일 조회 by Id
+     */
+    @GetMapping("/task/admin/grades/{id}")
+    GradeResponseForAdmin getGradeById(@PathVariable("id") Integer id);
+
+    /**
+     * 관리자 - 등급 전체 조회
+     */
+    @GetMapping("/task/admin/grades/list")
+    Page<GradeFeignResponse> getAllForAdmin(@RequestParam Pageable pageable);
+
+    /**
+     * 관리자 - 등급 등록
+     */
+    @PostMapping("/task/admin/grades")
+    GradeResponseForAdmin registerGrade(@RequestBody GradeRegisterRequest gradeRegisterRequest);
+
+    /**
+     * 관리자 - 등급 수정
+     */
+    @PutMapping("/task/admin/grades/{id}")
+    GradeResponseForAdmin modifyGrade(@PathVariable Integer id, @RequestBody GradeModifyRequest gradeModifyRequest);
 }
